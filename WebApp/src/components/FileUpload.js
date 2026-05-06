be45156caa8e5796e1b7d8d76c5d61785b625bc9
HaurisC <chodhryharis@gmail.com>
2026-05-06 18:33:05 -0400

    demo video blue header

--- WebApp/src/components/FileUpload.js
+++ WebApp/src/components/FileUpload.js
@@ -99,7 +99,7 @@ const FileUpload = () => {
             fontFamily: 'Arial, sans-serif',
         }}>
             <div style={{
-                backgroundColor: '#b4d3b2',
+                backgroundColor: '#4a90d9',
                 width: '100%',
                 height: '70px',
                 display: 'flex',
